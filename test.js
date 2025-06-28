c4c080d130f459ac38f109b56b2c920a7552d314
Dheeraj <prajapatidheeraj25042004@gmail.com>
2025-06-28 17:01:15 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
 async function hello(){
     console.log("Hello");
 }
+async function world(){
+    console.log(" World!");
+}
+async function helloWorld(){
+    await hello();
+    await world();
+}
 
-hello();
+helloWorld();
